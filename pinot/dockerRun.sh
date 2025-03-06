27df3ef8467ddfdb6c35005e5ea69e2fb040320a
saurabhlambe <saurabh.lambe10@gmail.com>
2025-03-06 13:20:29 +0530

    Some formatting changes to the Pinot script

--- pinot/dockerRun.sh
+++ pinot/dockerRun.sh
@@ -11,9 +11,9 @@ docker pull --platform linux/arm64 zookeeper >> /dev/null 2>&1
 echo "***************************************************"
 sleep 2
 
-echo "Deploying Zookeeper to run on port 2181"
+echo "Deploying Zookeeper on port 2181"
 docker run --network=pinot-demo --name pinot-zookeeper --restart always -p 2181:2181 --platform linux/arm64 -d zookeeper:3.5.9 >> /dev/null 2>&1
 sleep 2
 
-echo "Deploying Apache Pinot docker image to run on port 9000"
+echo "Deploying Apache Pinot docker image on port 9000"
 docker run -p 9000:9000 --name apache-pinot apachepinot/pinot:latest-arm64 QuickStart -type batch >> /dev/null 2>&1
