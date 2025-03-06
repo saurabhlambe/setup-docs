3c368d8824a5f5f607085b60da7e341018280817
saurabhlambe <saurabh.lambe10@gmail.com>
2025-03-06 13:10:20 +0530

    Some formatting changes to the Pinot script

--- pinot/dockerRun.sh
+++ pinot/dockerRun.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
-echo "This script assumes you have Docker engine installed and running. If not, please refer to https://docs.docker.com/engine/"
+
+# This script assumes you have Docker engine installed and running. If not, please refer to https://docs.docker.com/engine/"
+
 echo "Acquiring the latest Pinot image from https://github.com/apache/pinot"
 docker pull apachepinot/pinot:latest-arm64 >> /dev/null 2>&1
 sleep 2
+
 echo "Pulling the latest Zookeeper image from https://hub.docker.com/_/zookeeper"
 docker pull --platform linux/arm64 zookeeper >> /dev/null 2>&1
 echo "***************************************************"
 sleep 2
+
 echo "Deploying Zookeeper to run on port 2181"
 docker run --network=pinot-demo --name pinot-zookeeper --restart always -p 2181:2181 --platform linux/arm64 -d zookeeper:3.5.9 >> /dev/null 2>&1
 sleep 2
+
 echo "Deploying Apache Pinot docker image to run on port 9000"
 docker run -p 9000:9000 --name apache-pinot apachepinot/pinot:latest-arm64 QuickStart -type batch >> /dev/null 2>&1
-echo "***************************************************"
-echo "Pinot is deployed successfully. Browse to http://localhost:9000 to play around in the query console"
