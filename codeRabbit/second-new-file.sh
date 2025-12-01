8b5853e1beab2ed37600e14385e7d72b8431885f
Saurabh Lambe <saurabh.lambe10@gmail.com>
2025-12-01 15:37:49 +0530

    Created 2 new files this time to test 'Fixes All' feature

---
+++ codeRabbit/second-new-file.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+echo "created yet another new file"
+echo "testing the "Fix all" feature"
---
+++ codeRabbit/third-new-file.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+export SPOTIFY_CLIENT_ID="malksdjflkajwioe4rhj899234r"
+echo "Spotify client ID set."
